0900b5b4facc7097df9d0be39c406e62979ce3c9
Porygon Z and Eye of Truth <lj.garciam1@uniandes.edu.co>
2022-03-11 02:35:58 -0500

    Docs

--- Clientmod/main.go
+++ Clientmod/main.go
@@ -10,14 +10,18 @@ import (
 	"strings"
 )
 
+//Constantes del cliente
 const (
 	protocol = "tcp"
 )
 
+//mapa de canales local
 var channels map[string]struct{}
 
+//canal para sincronizar procesos
 var synchro chan string
 
+//ruta donde se encuentran los archivos del cliente
 var fileRoute string
 
 func main() {
@@ -27,6 +31,7 @@ func main() {
 	fmt.Println("Type client name: ")
 	cname, _ := reader.ReadString('\n')
 	fileRoute = strings.TrimSuffix("files_"+cname, "\n")
+	//Se inicializa el directorio de archivos
 	_ = os.Mkdir(fileRoute, 0755)
 	fmt.Println("Type server address: ")
 	address, _ := reader.ReadString('\n')
@@ -40,7 +45,9 @@ func main() {
 	}
 	socket.Write([]byte(cname))
 	fmt.Println("Connected succesfully to server @" + address)
+	//Inicializar mapa de canales
 	channels = make(map[string]struct{})
+	//Inicializar canal de sincronización
 	synchro = make(chan string)
 	//Mensaje inicial
 	fmt.Println("Standing by. Type help to recieve a list of commands.")
@@ -48,17 +55,20 @@ func main() {
 	go clientLoop(*reader, socket)
 	//Se escucha al servidor
 	socketReader := bufio.NewReader(socket)
-	//go serverListener(*socketReader)
 	for {
+		//Se obtiene un mensaje del servidor y se procesa
 		message, _ := socketReader.ReadBytes('\n')
 		serverRelay := strings.Fields(strings.TrimSuffix(string(message), "\n"))
 		log.Print("Server relay: " + string(message))
 		switch serverRelay[0] {
 		case "nochan":
+			//sincronizar con load() caso 1
 			synchro <- string(serverRelay[0])
 		case "chan":
+			//sincronizar con load() caso 2
 			synchro <- string(serverRelay[0])
 		case "file":
+			//descarga un archivo del servidor
 			fmt.Println("Downloading file " + serverRelay[1] + " from server.")
 			loadSize, _ := strconv.Atoi(serverRelay[2])
 			payload := make([]byte, loadSize)
@@ -74,49 +84,61 @@ func main() {
 
 //loop de lógica del cliente
 func clientLoop(reader bufio.Reader, socket net.Conn) {
+	//Se lee el input del usuario
 	input, _ := reader.ReadString('\n')
 	input = strings.TrimSuffix(input, "\n")
 	inputFields := strings.Fields(input)
 	switch inputFields[0] {
 	//Suscribirse a un canal
 	case "sub":
-		//Se revisa si ya se está suscrito al canal
 		if len(inputFields) == 1 {
+			//Avisar si faltan argumentos
 			fmt.Println("Not enough arguments.")
 		} else if _, ok := channels[inputFields[1]]; ok {
+			//Se revisa si ya se está suscrito al canal
 			fmt.Println("Already subscribed to " + inputFields[1] + ".")
 		} else {
+			//Se envía el comando al servidor
 			socket.Write([]byte(input + "\n"))
+			//Se guarda el canal en el mapa local
 			channels[inputFields[1]] = struct{}{}
 			fmt.Println("Subscribed to " + inputFields[1] + " succesfully.")
 		}
 	//Retirar suscripción a un canal
 	case "unsub":
-		//Se revisa si se está suscrito al canal
 		if len(inputFields) == 1 {
+			//Avisar si faltan argumentos
 			fmt.Println("Not enough arguments.")
 		} else if _, ok := channels[inputFields[1]]; ok {
+			//Se envía el comando al servidor
 			socket.Write([]byte(input + "\n"))
+			//Se actualiza el mapa local
 			delete(channels, inputFields[1])
 			fmt.Println("Unsubscribed from " + inputFields[1] + " succesfully.")
 		} else {
+			//Se revisa si no se está suscrito al canal
 			fmt.Println("Not subscribed to " + inputFields[1] + ".")
 		}
 	//Enviar un archivo
 	case "load":
 		if len(inputFields) <= 2 {
+			//Avisar si faltan argumentos
 			fmt.Println("Not enough arguments.")
 		} else {
+			//Se lee el archivo
 			payload, err := os.ReadFile(fileRoute + "/" + inputFields[2])
 			if err != nil {
 				fmt.Println("Error loading file: " + err.Error())
 			} else {
+				//Se manda el comando al servidor
 				socket.Write([]byte(input + " " + strconv.Itoa(len(payload)) + "\n"))
 				log.Println("Load order for " + inputFields[2] + " sent.")
-
+				//Se espera respuesta del servidor por medio del canal de sincronización
 				if relay := <-synchro; relay == "nochan" {
+					//Caso de que no exista el canal
 					fmt.Println("Error: No such channel exists.")
 				} else {
+					//Se envia el archivo al servidor
 					socket.Write(payload)
 					log.Println("File " + inputFields[2] + " sent.")
 					fmt.Println("File loaded to channel " + inputFields[1])
@@ -127,6 +149,7 @@ func clientLoop(reader bufio.Reader, socket net.Conn) {
 	//Ver lista de comandos
 	case "help":
 		fmt.Println("COMMAND LIST \n sub chan - Subscribes to \"chan\" channel.\n unsub chan - Unubscribes from \"chan\" channel.\n load chan file - Loads \"file\" from user's subfolder into \"chan\" channel.\n exit - Exits the application.\n help - Displays information on available commands.")
+	//Salir de la aplicación
 	case "exit":
 		socket.Close()
 		os.Exit(0)
@@ -134,6 +157,6 @@ func clientLoop(reader bufio.Reader, socket net.Conn) {
 	default:
 		fmt.Println("Unknown command. Type help to recieve a list of commands.")
 	}
-
+	//Se repite lo anterior hasta que se cierre la conexión
 	clientLoop(reader, socket)
 }
---
+++ README.md
@@ -0,0 +1,28 @@
+GO Fortune Engine
+----------------------
+A simple CLI file server written in Golang as a learning excercise. 
+The Clientmod module represents a client application, and the Servermod handles client connections. 
+The server runs on localhost:25565 by default.
+
+FortuneUI is a Vue.js GUI to visualize server operations. It consumes the server's API, available at localhost:25566 while the server is running.
+
+Client commands:
+sub chan - Subscribe to 'chan' server channel
+unsub chan - Unsubscribe from 'chan' server channel
+load chan file - Loads 'file' file from the client's file directory into 'chan' channel. The server will send this file to all clients currently subscribed to 'chan'
+help - Displays information on commands
+exit - Take a wild guess
+
+---------------------
+Servidor de archivos de CLI escrito en Golang para aprender.
+El módulo Clientmod representa la aplicación del cliente, y Servermod administra las conexiones a los clientes.
+El servidor se ejecuta en localhost:25565 por defecto.
+
+FortuneUI es una GUI en Vue.js para visualizar las operaciones del servidor. Consume el API del servidor desde localhost:25566 mientras este se encuentre en ejecución.
+
+Comandos del cliente:
+sub chan - Suscribirse al canal 'chan' del servidor
+unsub chan - Retirar suscripción del canal 'chan' del servidor
+load chan file - Carga el archivo 'file' del directorio de archivos del cliente al canal 'chan' del servidor. El servidor distribuye el archivo a todos los clientes suscritos a 'chan'
+help - Mustra información sobre los comandos
+exit - Adivine
