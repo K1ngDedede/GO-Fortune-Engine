b8df56449464d82b18fd9106f7091dc2ac901ace
Porygon Z and Eye of Truth <lj.garciam1@uniandes.edu.co>
2022-03-11 00:48:54 -0500

    vue.js UI, docs

---
+++ Servermod/fortuneUI/src/App.vue
@@ -0,0 +1,211 @@
+<script setup lang="ts">
+import { onMounted, ref, watchEffect, type Ref } from 'vue'
+
+const API_URL = "http://localhost:25566/"
+
+const chandata = ref(null)
+const clients = ref(null)
+const files = ref(null)
+const chan = ref('')
+const autoUpdate = ref('')
+
+watchEffect(async () => {
+  fetchData()
+  setInterval(fetchData,10000)  
+})
+
+async function fetchData(){
+  chandata.value = await (await fetch(API_URL+"chan")).json()
+  clients.value = await (await fetch(API_URL+"clients")).json()
+  files.value = await (await fetch(API_URL+"files")).json()
+  
+}
+
+
+
+function nochan(channel: string){
+  if(chandata.value && Object.keys(chandata.value[channel]).length !=0){
+    return false
+  }else{
+    return true
+  }
+}
+
+function onlineClients(){
+  let c = []
+  let count = 0
+  for (let y of Object.keys(clients.value!)){
+    if(clients.value![y]["Online"]){
+      c[count] = y
+      count++
+    }    
+  }
+  return c
+}
+
+
+
+</script>
+
+<template>
+  <header>
+    <h1>GO Fortune Engine</h1> 
+    
+  </header>
+  <div>
+
+  </div>
+  <div>
+    <p>Currently online: {{onlineClients().length}}</p>
+    <br>
+    <br>
+    <br>
+    <h2>File Log</h2>
+      <table>
+        <tr>
+          <th>
+            Filename
+          </th>
+          <th>
+            Size
+          </th>
+          <th>
+            Channel
+          </th>
+          <th>
+            Sender
+          </th>
+          <th>
+            Recipients
+          </th>
+          <th>
+            Timestamp
+          </th>
+        </tr>
+        <tr v-for="y in files">
+          <td>
+            {{y["Fname"]}}
+          </td>
+          <td>
+            {{y["Fsize"]}}
+          </td>
+          <td>
+            {{y["Channel"]}}
+          </td>
+          <td>
+            {{y["Sender"]}}
+          </td>
+          <td>
+            {{y["Recipients"]}}
+          </td>
+          <td>
+            {{y["Tstamp"]}}
+          </td>
+        </tr>
+      </table>
+  </div>
+  
+  <main class="debug">    
+    <div>
+      <h2>Online Clients</h2>
+      {{onlineClients()}}
+      <br>
+      <br>
+      <br>
+      <h2>Open Channels</h2>
+      <select v-model="chan" multiple style="width:100%">
+        <option v-for="(value, name) in chandata">{{name}}</option>
+      </select>
+      <div v-if="chan">
+        <div v-if="nochan(chan)">
+          No clients in channel.
+        </div>
+        <div v-else>
+          <h4>Clients in {{ chan }}</h4> 
+          <ul>
+            <li v-for="value in chandata![chan]">{{value["Cname"]}} @ {{value["Address"]}}</li>
+          </ul>
+        </div> 
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+  color: #DEDFD7;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+h1, h2, h3, h4, h5, h6{
+  color: #EACD8E;
+}
+
+
+td, th {
+  border: 1px solid #7E92A1;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #7E92A1;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.debug{
+  
+  margin-left: 20%;
+}
+
+
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+    background-color: #4D6879;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
--- Servermod/main.go
+++ Servermod/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //Constantes del servidor
@@ -17,17 +20,42 @@ const (
 	protocol = "tcp"
 )
 
+//client representa el estado de una conexión que tiene el servidor con un cliente
 type client struct {
 	id         int
-	cname      string
-	address    string
+	Cname      string
+	Address    string
 	connection net.Conn
 	reader     bufio.Reader
-	sentfiles  int
 }
 
+//auxClient representa a un cliente
+type auxClient struct {
+	Sentfiles int
+	Online    bool
+}
+
+//simpleFile representa los datos de un archivo enviado por un cliente
+type simpleFile struct {
+	Fname      string
+	Fsize      int
+	Channel    string
+	Sender     string
+	Recipients []string
+	Tstamp     time.Time
+}
+
+//id de la conexión más reciente al servidor
 var bigId int
+
+//mapa de canales del servidor
 var channels map[string]map[int]client
+
+//estructura para almacenar datos de los archivos enviados
+var fileLog []simpleFile
+
+//estructura para almacenar datos de los clientes
+var clientLog map[string]auxClient
 
 func main() {
 	fmt.Println("Launching server @ " + hostname + ":" + port)
@@ -42,7 +70,12 @@ func main() {
 	defer socket.Close()
 	//Inicializar mapa de canales
 	channels = make(map[string]map[int]client)
-
+	//Inicializar log de archivos
+	fileLog = make([]simpleFile, 0)
+	//Inicializar log de clientes
+	clientLog = make(map[string]auxClient)
+	//Servir endpoints
+	go serve()
 	//Aceptar clientes
 	for {
 		//Se acepta la conexión
@@ -58,6 +91,7 @@ func main() {
 	}
 }
 
+//Fucnión que registra una nueva conexión al servidor
 func greetClient(c net.Conn) {
 	//Recibir información del cliente
 	bufferReader := bufio.NewReader(c)
@@ -68,42 +102,56 @@ func greetClient(c net.Conn) {
 		c.Close()
 		return
 	}
-	//Se inicializa el objeto cliente
-	newClient := client{id: bigId, cname: string(buffer[:len(buffer)-1]), address: c.RemoteAddr().String(), connection: c, reader: *bufferReader}
+	//Se inicializan los objetos cliente y conexión al cliente
+	newClient := client{id: bigId, Cname: string(buffer[:len(buffer)-1]), Address: c.RemoteAddr().String(), connection: c, reader: *bufferReader}
+	if aux, ok := clientLog[newClient.Cname]; !ok {
+		clientLog[newClient.Cname] = auxClient{Online: true}
+	} else {
+		aux.Online = true
+		clientLog[newClient.Cname] = aux
+	}
 	bigId += 1
+	//Se esperan comandos del nuevo cliente
 	go handleConnection(newClient)
 }
 
+//Función que se encarga de comunicarse con el cliente por medio de la conexión dada
 func handleConnection(c client) {
-
+	//Se obtiene un mensaje del cliente
 	bufferReader := c.reader
 	buffer, err := bufferReader.ReadBytes('\n')
-	//Se revisa si se desconectó el usuario
+	//Se revisa si se desconectó el cliente
 	if err != nil {
 		fmt.Println("Client left.")
 		c.connection.Close()
+		//Se retiran todas las suscripciones activas del cliente
 		go unsubAll(c)
+		//Se cierra el proceso de la conexión actual
 		return
 	}
+	//Se procesa el mensaje
 	bufferString := string(buffer[:len(buffer)-1])
 	bufferString = strings.TrimSuffix(bufferString, "\n")
 	bufferFields := strings.Fields(bufferString)
-	log.Println(c.cname+" command:", bufferString)
+	log.Println(c.Cname+" command:", bufferString)
+	//Se actua según el mensaje enviado
 	switch bufferFields[0] {
+	//suscribirse a un canal
 	case "sub":
 		subscribeClient(c, bufferFields[1])
+	//retirar suscripción a un canal
 	case "unsub":
 		unsubscribeClient(c, bufferFields[1])
+	//subir archivo a un canal
 	case "load":
 		loadSize, _ := strconv.Atoi(bufferFields[3])
 		sendFile(c, bufferFields[1], bufferFields[2], loadSize)
 	}
-
-	//c.connection.Write(buffer)
-	//fmt.Println(channels)
+	//Se repite lo anterior hasta que se cierre la conexión
 	handleConnection(c)
 }
 
+//Función que se encarga de suscribir un cliente a un canal
 func subscribeClient(c client, chanName string) {
 	if _, ok := channels[chanName]; !ok {
 		//Si no existe el canal, se crea
@@ -111,15 +159,17 @@ func subscribeClient(c client, chanName string) {
 	}
 	channels[chanName][c.id] = c
 	c.connection.Write([]byte("sub succesful\n"))
-	log.Println(c.cname + " subscribed to " + chanName + " channel.")
+	log.Println(c.Cname + " subscribed to " + chanName + " channel.")
 }
 
+//Función que se encarga de retirar la suscrpción de un cliente a un canal
 func unsubscribeClient(c client, chanName string) {
 	delete(channels[chanName], c.id)
 	c.connection.Write([]byte("unsub succesful\n"))
-	log.Println(c.cname + " unsubscribed from " + chanName + " channel.")
+	log.Println(c.Cname + " unsubscribed from " + chanName + " channel.")
 }
 
+//Función que se encarga de recibir un archivo de un cliente, y enviarlo a todos los clientes suscritos al canal especificado
 func sendFile(c client, chanName string, fileName string, fileSize int) {
 	if _, ok := channels[chanName]; !ok {
 		//Si no existe el canal, se le notifica al cliente
@@ -127,22 +177,90 @@ func sendFile(c client, chanName string, fileName string, fileSize int) {
 		log.Println("No channel " + chanName)
 	} else {
 		c.connection.Write([]byte("chan\n"))
-		log.Println("Authorized client " + c.cname + " for loading.")
-		//payload, _ := c.reader.ReadBytes('\n')
+		log.Println("Authorized client " + c.Cname + " for loading.")
+		//Se lee el archivo enviado por el cliente
 		payload := make([]byte, fileSize)
 		c.connection.Read(payload)
-		c.sentfiles += 1
+		//Se guardan datos de la transferencia
+		if aux, ok := clientLog[c.Cname]; ok {
+			aux.Sentfiles += 1
+			clientLog[c.Cname] = aux
+		}
+		sFile := simpleFile{Fname: fileName, Fsize: fileSize, Channel: chanName, Sender: c.Cname, Tstamp: time.Now(), Recipients: make([]string, 0)}
+		//Se envia el archivo a todos los clientes suscritos al canal
 		for _, y := range channels[chanName] {
 			y.connection.Write([]byte("file " + fileName + " " + strconv.Itoa(fileSize) + "\n"))
 			y.connection.Write(payload)
-			log.Println("Sent " + fileName + " to client " + y.cname + " @" + y.address)
+			sFile.Recipients = append(sFile.Recipients, y.Cname)
+			log.Println("Sent " + fileName + " to client " + y.Cname + " @" + y.Address)
 		}
+		fileLog = append(fileLog, sFile)
 	}
 }
 
+//Función encargada de retirar todas las suscripciones de un cliente
 func unsubAll(c client) {
 	for _, chann := range channels {
 		delete(chann, c.id)
 	}
-	log.Println("Unsubscribed client " + c.cname + " from all channels.")
+	if aux, ok := clientLog[c.Cname]; ok {
+		aux.Online = false
+		clientLog[c.Cname] = aux
+	}
+	log.Println("Unsubscribed client " + c.Cname + " from all channels.")
+}
+
+//Función encargada de servir los endpoints del API para el GUI web
+func serve() {
+	http.HandleFunc("/lmao", hello)
+	http.HandleFunc("/chan", chanpoint)
+	http.HandleFunc("/files", filepoint)
+	http.HandleFunc("/clients", clientpoint)
+	http.ListenAndServe(":25566", nil)
+}
+
+//endpoint de pruebas
+func hello(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		fmt.Fprintf(w, "lmao")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+}
+
+//endpoint GET de canales
+func chanpoint(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		data, _ := json.Marshal(channels)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fmt.Fprint(w, string(data))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+//endpoint GET de información de archivos
+func filepoint(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		data, _ := json.Marshal(fileLog)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fmt.Fprint(w, string(data))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+//endpoint GET de información de clientes
+func clientpoint(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		data, _ := json.Marshal(clientLog)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fmt.Fprint(w, string(data))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
